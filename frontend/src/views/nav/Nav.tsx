162ba0dc531faf3f169f1bcd6d00fdbe6b325b0e
Jan Nyberg <jan.nyberg@gmail.com>
2023-11-10 21:40:01 +0200

    FE app named

--- frontend/src/views/nav/Nav.tsx
+++ frontend/src/views/nav/Nav.tsx
@@ -1,11 +1,11 @@
-import {ModeToggle} from '@/components/theme-toggle';
+import { ModeToggle } from '@/components/theme-toggle';
 import './Nav.scss';
 
 const Nav = () => {
     return (
         <nav className="flex items-center justify-between align-center p-4">
             <div className="flex items-center">
-                <h3 className="m-0 pl-2">BBlog</h3>
+                <h3 className="m-0 pl-2">TRST</h3>
             </div>
             <div className="flex items-center">
                 <ModeToggle />
