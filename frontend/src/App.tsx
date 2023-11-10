683ee100dc3dbbf3e0a275b0f2f010fcb75820e7
Jan Nyberg <jan.nyberg@gmail.com>
2023-11-10 21:33:00 +0200

    Added vite FE

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+import Demo from './views/content/Demo';
+import {ThemeProvider} from './components/theme-provider';
+import Nav from './views/nav/Nav';
+import './App.css';
+import './globals.css';
+
+const App = () => {
+    return (
+        <ThemeProvider
+            defaultTheme="dark"
+            storageKey="vite-ui-theme"
+        >
+            <Nav />
+            <Demo />
+        </ThemeProvider>
+    );
+};
+
+export default App;
---
+++ frontend/src/views/nav/Nav.tsx
@@ -0,0 +1,17 @@
+import {ModeToggle} from '@/components/theme-toggle';
+import './Nav.scss';
+
+const Nav = () => {
+    return (
+        <nav className="flex items-center justify-between align-center p-4">
+            <div className="flex items-center">
+                <h3 className="m-0 pl-2">BBlog</h3>
+            </div>
+            <div className="flex items-center">
+                <ModeToggle />
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
