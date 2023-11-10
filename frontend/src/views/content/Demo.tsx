fd23acee686ceb40dcae9a98efb523f07761a46b
Jan Nyberg <jan.nyberg@gmail.com>
2023-11-11 00:48:52 +0200

    Working mock send, show and clear prompt loop

--- frontend/src/views/content/Demo.tsx
+++ frontend/src/views/content/Demo.tsx
@@ -1,43 +1,66 @@
-import { useState } from 'react';
-import { Textarea } from "@/components/ui/textarea"
-import { Button } from '@/components/ui/button';
-import ValidateOutput, { OutputData, SentenceData } from './ValidatedOutput';
-import dummy from './dummy.json'
+import {useState} from 'react';
+import {Textarea} from '@/components/ui/textarea';
+import {Button} from '@/components/ui/button';
+import ValidateOutput, {SentenceData} from './ValidatedOutput';
+import {Skeleton} from '@/components/ui/skeleton';
+import dummy from './dummy.json';
 import './Content.scss';
 
 const Demo = () => {
-    const [prompt, setPrompt] = useState("");
+    const [prompt, setPrompt] = useState('');
+    const [fetchingResults, setFetchingResults] = useState(false);
     const [result, setResult] = useState<SentenceData[] | null>(null);
 
     const handlePromptChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setPrompt(e.target.value);
-    }
+    };
 
     const sendPrompt = () => {
-        if (dummy) {
-            setResult(dummy);
-        }
+        setFetchingResults(true);
+        setTimeout(() => {
+            if (dummy) {
+                setResult(dummy);
+                setFetchingResults(false);
+            }
+        }, 2000);
+    };
 
-    }
+    const clearResults = () => {
+        setResult(null);
+    };
 
     return (
         <>
-            <div className='main-container'>
-                {!result ?
-                    < div className='chat-container'>
-                        <Textarea rows={10} placeholder="Type your message here." value={prompt} onChange={(e) => handlePromptChange(e)} />
-                        <Button
-                            className="float-right mt-3"
-                            onClick={sendPrompt}
-                        // disabled={prompt === ""}
-                        >Send</Button>
-                    </div>
-                    :
-                    <ValidateOutput data={result} />
-                }
-
-
-            </div >
+            <div className="main-container">
+                {!result ? (
+                    <>
+                        {!fetchingResults ? (
+                            <div className="chat-container">
+                                <Textarea
+                                    rows={10}
+                                    placeholder="Type your message here."
+                                    value={prompt}
+                                    onChange={(e) => handlePromptChange(e)}
+                                />
+                                <Button
+                                    className="float-right mt-3"
+                                    onClick={sendPrompt}
+                                    // disabled={prompt === ""}
+                                >
+                                    Send
+                                </Button>
+                            </div>
+                        ) : (
+                            <Skeleton className="fetching-skeleton" />
+                        )}
+                    </>
+                ) : (
+                    <ValidateOutput
+                        data={result}
+                        clear={clearResults}
+                    />
+                )}
+            </div>
         </>
     );
 };
--- frontend/src/views/content/ValidatedOutput.tsx
+++ frontend/src/views/content/ValidatedOutput.tsx
@@ -1,3 +1,5 @@
+import {Button} from '@/components/ui/button';
+
 export interface SentenceData {
     sentence: string;
     trust_score: number;
@@ -6,36 +8,66 @@ export interface SentenceData {
 
 export interface OutputData {
     data: SentenceData[];
+    clear: () => void;
 }
 
-const ValidateOutput = ({ data }: OutputData) => {
-
-    const getHighlightStyle = (trustScore: number) => {
-        const score = trustScore;
-
-        if (score >= 0.75) {
-            return { borderBottom: '2px solid green' };
-        } else if (score >= 0.5) {
-            return { borderBottom: '2px solid yellow' };
+const ValidateOutput = ({data, clear}: OutputData) => {
+    const perc2color = (perc: number) => {
+        perc = perc * 100;
+        let r,
+            g,
+            b = 0;
+        if (perc < 50) {
+            r = 255;
+            g = Math.round(5.1 * perc);
         } else {
-            return { borderBottom: '2px solid red' };
+            g = 255;
+            r = Math.round(510 - 5.1 * perc);
         }
+        return {
+            borderBottom: `4px solid rgba(${r}, ${g}, ${b}, 1)`,
+        };
     };
+
+    // const getHighlightStyle = (trustScore: number) => {
+    //     const score = trustScore;
+
+    //     if (score >= 0.75) {
+    //         return {borderBottom: '2px solid green'};
+    //     } else if (score >= 0.5) {
+    //         return {borderBottom: '2px solid yellow'};
+    //     } else {
+    //         return {borderBottom: '2px solid red'};
+    //     }
+    // };
+
     return (
         <>
             <div className="output-container">
                 <div>
                     <p className="output-p">
                         {data.map((item, index) => (
-                            <span key={index} style={getHighlightStyle(item.trust_score)}>
+                            <span
+                                onClick={() => window.alert(item.trust_score)}
+                                className="output-span"
+                                key={index}
+                                style={perc2color(item.trust_score)}
+                            >
                                 {item.sentence}{' '}
                             </span>
                         ))}
                     </p>
                 </div>
+                <Button
+                    className="float-right mt-3"
+                    onClick={() => clear()}
+                    // disabled={prompt === ""}
+                >
+                    Clear
+                </Button>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default ValidateOutput;
